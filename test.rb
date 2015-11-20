e4d21249a2672fc125a07cc564f070292d4f4ba8
unknown <rcooper2@CMH-MIS-DWL3YV1.cmh.netjets.com>
2015-11-20 16:43:56 -0500

    initial commit

---
+++ test.rb
@@ -0,0 +1,21 @@
+# I am a comment. Just ignore me.
+
+puts "Hello Ruby!"
+
+puts "This is main Ruby Program"
+
+END {
+   puts "Initializing Ruby Program 5"
+}
+
+END {
+   puts "Initializing Ruby Program 6"
+}
+
+BEGIN {
+   puts "Initializing Ruby Program 1"
+}
+
+BEGIN {
+   puts "Initializing Ruby Program 2"
+}
